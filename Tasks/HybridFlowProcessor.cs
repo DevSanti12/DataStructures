303bc5deb7b9c5922b1ec34591c8a63025c677d3
Santiago Leyva <99155189+DevSanti12@users.noreply.github.com>
2025-05-19 22:00:52 -0500

    Added HybridFlowProcessor logic

--- Tasks/HybridFlowProcessor.cs
+++ Tasks/HybridFlowProcessor.cs
@@ -1,28 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using Tasks.DoNotChange;
 
 namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        private LinkedList<T> _storage = new LinkedList<T>();
+
+        /// <summary>
+        /// FILO behavior: Push adds an item to the top of the storage.
+        /// </summary>
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (_storage.Count == 0)
+            {
+                throw new InvalidOperationException("The proccesor is empty. There is not item to dequeue. ");
+            }
+            T value = _storage.First.Value; //get the value from the first node
+            _storage.RemoveFirst();
+            return value;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            _storage.AddLast(item); // Add to the end of the doubly linked list.
         }
 
+        /// <summary>
+        /// FILO behavior: Pop retrieves the topmost item (from the beginning) and removes it.
+        /// </summary>
         public T Pop()
         {
-            throw new NotImplementedException();
+            if(_storage.Count == 0)
+            {
+                throw new InvalidOperationException("The processor is empty. There is no item to pop");
+            }
+
+            T value = _storage.First.Value;
+            _storage.RemoveFirst(); //Removes the first node
+            return value;
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            _storage.AddFirst(item); //Add to the beginning of the doubly linked list.
         }
     }
 }
